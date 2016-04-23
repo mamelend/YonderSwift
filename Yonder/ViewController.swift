b8967bceba0a2e684a3077d1d2d02ea7b3c7c9b0
Miguel Melendez <m.melend@gmail.com>
2016-04-23 16:51:59 -0400

    Initial Commit

---
+++ Yonder/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Yonder
+//
+//  Created by Miguel Melendez on 4/23/16.
+//  Copyright © 2016 Miguel Melendez. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
